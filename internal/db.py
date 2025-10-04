a980001897692849f92747d67a4d6efdce88f608
dyaksa <diasnour0395@gmail.com>
2025-10-04 10:20:32 +0700

    config connection db

--- internal/db.py
+++ internal/db.py
@@ -3,10 +3,27 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from core.config import settings
 from sqlalchemy import event
 from rag.embbedings import from_blob
+from sqlalchemy.pool import NullPool
 import numpy as np
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    settings.DATABASE_URL, 
+    poolclass=NullPool,
+    connect_args={
+        "check_same_thread": False,
+        "timeout": 10,
+    },
+    future=True,
+    echo=False,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+    future=True,
+)
 Base = declarative_base()
 
 
